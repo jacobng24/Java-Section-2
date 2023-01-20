89065eb882c1de953c6eecb13b0f3220e5539b75
Jacob <110934976+jacobng24@users.noreply.github.com>
2023-01-19 20:07:28 -0700

    Update HCS_2_2.java

--- HCS_2_2.java
+++ HCS_2_2.java
@@ -3,7 +3,16 @@ public class HCS_2_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+		Spinner s1 = new Spinner(5);
+		System.out.println(s1.Spin());
+		System.out.println(s1.Spin());
+		System.out.println(s1.Spin());
+		System.out.println(s1.Spin());
+		System.out.println(s1.Spin());
+		System.out.println(s1.Spin());
+		System.out.println(s1.Spin());
+		System.out.println(s1.Spin());
+		System.out.println(s1.currentRun());
 	}
-
+	
 }
